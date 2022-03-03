5c04e04310740d61f0d26fafe28df6b79c5a1c3a
Leonardo Oliveira <leozartino@gmail.com>
2022-03-03 19:14:05 -0300

    feat: add Person class (abastract)

--- WebApi_Project/Models/Person.cs
+++ WebApi_Project/Models/Person.cs
@@ -6,8 +6,8 @@
         public string Name { get; set; }
         public string Birth_date { get; set; }
         public string Email { get; set; }
-        public Class ClassObject { get; set; }
-        public int Class_id { get; set; }
+        public Class Class { get; set; }
+        public int ClassId { get; set; }
 
     }
 }
