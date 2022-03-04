79b982db6b2931ca50895c55788f5943e8e07315
Leonardo Oliveira <leozartino@gmail.com>
2022-03-03 21:03:12 -0300

    feat: add constructor context

--- WebApi_Project/Context/AppDbContext.cs
+++ WebApi_Project/Context/AppDbContext.cs
@@ -4,7 +4,12 @@ using WebApi_Project.Models;
 namespace WebApi_Project.Context
 {   //O contexto vai encapsular todas as nossas entidades 
     public class AppDbContext : DbContext
-    {   
+    {    
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
         // Existe um padrão quando a gente usa o EntityFramework
         // para nomear listas no plural
         public DbSet<Class> Classes { get; set; }
@@ -12,7 +17,7 @@ namespace WebApi_Project.Context
         public DbSet<Professor> Professors {get;set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuild)
-        {
+        {    
             // Posso mudar o nome do banco e no caso do windows devmeos passar o @ para forçar o interpreter entender a string com backslash
             optionsBuild.UseSqlServer(@"Password=1525@Xyzt;Persist Security Info=True;User ID=sa;Initial Catalog=first_database;Data Source=DESKTOP-9P4JB41\SQLEXPRESS");
         }
