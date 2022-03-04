50644e13143590054ab0bcba236585b96857e365
Leonardo Oliveira <leozartino@gmail.com>
2022-03-04 11:00:22 -0300

    feat: add user model and controller and static data

---
+++ WebApi_Project/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi_Project.Models;
+
+namespace WebApi_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("index-all")]
+        public async Task<ActionResult<List<Student>>> GetAllStudents()
+        {
+            var classObject = new Class { Id = 1, Module = "5th", Name = "Turma01" };
+            
+
+            var students = new List<Student>
+            {
+                new Student { 
+                    Id = 1, 
+                    Name = "Leonardo Oliveira", 
+                    Birth_date = "15/10/1996", 
+                    Email = "leo@email.com",
+                    Class = classObject,
+                    ClassId = 1, 
+                    Hobby = "Nadar" 
+                }
+            };
+
+            return Ok(students);
+        }
+    }
+}
+    
---
+++ WebApi_Project/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi_Project.Models;
+
+namespace WebApi_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> users = new()
+        {
+                new User {
+                    Id = 1,
+                    Name = "Leonardo Oliveira",
+                    Email = "leo@email.com",
+                    Password = "8420@xkl",
+                    IsAdmin = true,
+                },
+                new User {
+                    Id = 2,
+                    Name = "Maria Silva",
+                    Email = "maria@email.com",
+                    Password = "123@kcx",
+                    IsAdmin = true,
+                },
+                new User
+                {
+                    Id = 3,
+                    Name = "João Pereira",
+                    Email = "joao@email.com",
+                    Password = "456@xkl",
+                    IsAdmin = false,
+                }
+            };
+
+        [HttpGet]
+        [Route("index-all")]
+        //o c# não suporta conversão Implicita de operadores em Interface
+        // Entao voce pode usar List(concreto) ou um IEnumerable mas no final
+        // usar o .ToList() se caso eu usar um dbContext
+        public ActionResult<IEnumerable<User>> GetAllUser()
+        {
+
+            return Ok(users);
+        }
+
+        [HttpPost]
+        [Route("post-user")]
+        public ActionResult<User> PostUser([FromBody]User user)
+        {
+            users.Add(user);
+            return Ok();
+
+        }
+    }
+}
