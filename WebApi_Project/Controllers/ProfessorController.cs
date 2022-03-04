5034e565a2ab436dff40008cfb10c7224beb62c9
Leonardo Oliveira <leozartino@gmail.com>
2022-03-03 21:03:26 -0300

    feat: add professor controller

---
+++ WebApi_Project/Controllers/ProfessorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi_Project.Context;
+using WebApi_Project.Models;
+
+namespace WebApi_Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfessorController: ControllerBase
+
+    {
+        private readonly AppDbContext _appDbContext;
+        
+        public ProfessorController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        [Route("professors")]
+        public async Task<ActionResult<IEnumerable<Professor>>> GetProfessors()
+        {
+            return Ok(await _appDbContext.Professors.ToListAsync());
+
+        }
+
+
+    }
+}
