a7138881b83f35107fbe41fe87232e2f2d654a85
Leonardo Oliveira <leozartino@gmail.com>
2022-03-06 22:36:44 -0300

    feat: add authcontroller and usercontroller

---
+++ WebApi_Project/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Cryptography;
+using WebApi_Project.Context;
+using WebApi_Project.Dto;
+using WebApi_Project.Models;
+using WebApi_Project.Services;
+
+namespace WebApi_Project.Controllers
+{
+  [Route("api/auth-controller")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly AppDbContext _appDbContext;
+
+    public AuthController(AppDbContext appDbContext)
+    {
+      _appDbContext = appDbContext;
+    }
+
+
+    [HttpPost]
+    [Route("login")]
+    public ActionResult<dynamic> LoginUser(UserLoginDto userLogin)
+    {
+      var user = _appDbContext.Users.Where((User u) => u.Username == userLogin.Username).FirstOrDefault(); ;
+
+
+
+      if (user == null)
+      {
+        return NotFound(new { message = "User not found!" });
+      }
+
+
+      if (!VerifyPasswordHash(userLogin.Password, user.PasswordHash, user.PasswordSalt))
+      {
+        return BadRequest(new { message = "Username and/or password are incorrect!" });
+      }
+
+      //Ger o token
+      var token = TokenService.GenerateToken(user);
+
+      var userResponse = new
+      {
+        user.Id,
+        user.Name,
+        user.Username,
+        user.Role,
+      };
+
+      return new
+      {
+        user = userResponse,
+        token,
+      };
+
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+      using (var hmac = new HMACSHA512(passwordSalt))
+      {
+        var hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        return hash.SequenceEqual(passwordHash);
+      };
+
+
+    }
+
+
+  }
+}
---
+++ WebApi_Project/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using WebApi_Project.Context;
+using WebApi_Project.Dto;
+using WebApi_Project.Models;
+
+namespace WebApi_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public UsersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        [Route("create-user")]
+        public async Task<ActionResult<User>> CreateUser([FromBody] UserDto userDto)
+        {
+            CreatePasswordHash(userDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            Guid id = Guid.NewGuid();
+
+
+            User user = new ()
+            {
+                Id = id.ToString(),
+                Username = userDto.Username,
+                Email = userDto.Email,
+                Name = userDto.Name,
+                Role = "normal",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            };
+
+             _appDbContext.Users.Add(user);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        [Route("index-all")]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return Ok(await _appDbContext.Users.ToListAsync());
+        }
+
+
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
